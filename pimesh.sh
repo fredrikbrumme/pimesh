0f344d109065ca64c7b85d0176237be1bf90e745
Fredrik <fredrik@iMac.local>
2017-07-01 16:20:08 +0200

    Download and build oslrd

--- pimesh.sh
+++ pimesh.sh
@@ -7,8 +7,21 @@ echo "This will setup the Pi as a wifi OLSR mesh client/gateway"
 while true; do
   read -p "Do you wish to continue?" yn
     case $yn in
-      [Yy]* ) make install; break;;
+      [Yy]* ) break;;
       [Nn]* ) exit;;
       * ) echo "Please answer yes or no.";;
     esac
 done
+
+# Build OLSRd
+cd /tmp
+if [ ! -d olsrd/.git ]
+then
+  git clone https://github.com/OLSR/olsrd.git
+else
+  cd olsrd
+  git pull
+fi
+cd /tmp/olsrd
+make
+cd -
